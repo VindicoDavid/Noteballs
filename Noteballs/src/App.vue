12caed50609452c82c94ba833ac7536ef405c4d3
David Diaz Clifton <daviddiaz-clifton@vindico.net>
2024-07-30 18:34:53 +0200

    Added bulma CSS

--- Noteballs/src/App.vue
+++ Noteballs/src/App.vue
@@ -4,9 +4,13 @@
 
 <template>
   <RouterView/>
-  <RouterLink to="/stats">Stats</RouterLink>|
+  <RouterLink to="/stats" class="button">Stats</RouterLink>|
 
-  <RouterLink to="/">Notes</RouterLink>
+  <RouterLink to="/" class="button">Notes</RouterLink>
 
 </template>
+
+<style>
+@import 'bulma/css/bulma.min.css'
+</style>
 
