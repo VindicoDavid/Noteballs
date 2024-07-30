4f15ba0edf77bba582328792cdc022bee721a79f
David Diaz Clifton <daviddiaz-clifton@vindico.net>
2024-07-30 18:53:31 +0200

    78 add navbar initial

--- Noteballs/src/App.vue
+++ Noteballs/src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
+import NavBar from '@/components/Layout/NavBar.vue'
+
 
 </script>
 
 <template>
+    <NavBar/>
   <RouterView/>
-  <RouterLink to="/stats" class="button">Stats</RouterLink>|
+  <!-- <RouterLink to="/stats" class="button">Stats</RouterLink>|
 
-  <RouterLink to="/" class="button">Notes</RouterLink>
+  <RouterLink to="/" class="button">Notes</RouterLink> -->
+
+
 
 </template>
 
---
+++ Noteballs/src/components/Layout/NavBar.vue
@@ -0,0 +1,60 @@
+ <template>
+
+<nav class="navbar is-success" role="navigation" aria-label="main navigation">
+ 
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+        <a class="navbar-item is-size-4 is-family-monospace">
+            Noteballs
+            </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+       
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item is-selected">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
+    </div>
+
+    
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+       
+        <RouterLink 
+        to="/"
+        class="navbar-item"
+        active-class="is-active">
+
+        Notes
+        </RouterLink>
+
+        <RouterLink 
+        to="/stats"
+        class="navbar-item"
+        active-class="is-active"
+        >
+        Stats
+
+        </RouterLink>
+
+        </div>
+      </div>
+    </div>
+
+</nav>
+ </template>
