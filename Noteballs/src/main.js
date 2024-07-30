ff3a626fbb74012864e542d2d2b2569ba56f6a25
David Diaz Clifton <daviddiaz-clifton@vindico.net>
2024-07-30 18:05:09 +0200

    new components and corresponding routes

--- Noteballs/src/main.js
+++ Noteballs/src/main.js
@@ -4,10 +4,22 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import {createRouter, createWebHashHistory} from 'vue-router'
 import App from './App.vue'
+import ViewNotes from '@/views/ViewNotes.vue'
+import ViewStats from '@/views/ViewStats.vue'
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
+        {
+            path: '/',
+            name: 'notes',
+            component: () => import('@/views/ViewNotes.vue')
+        },
+        {
+            path: '/',
+            name: 'stats',
+            component: () => import('@/views/ViewStats.vue')
+        },
 
     ]
 })
