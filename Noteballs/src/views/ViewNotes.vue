eafa0062516d2944a5b160c0c78103a79e18ec53
David Diaz Clifton <daviddiaz-clifton@vindico.net>
2024-07-31 12:03:13 +0200

    Update notes

--- Noteballs/src/views/ViewNotes.vue
+++ Noteballs/src/views/ViewNotes.vue
@@ -1,38 +1,56 @@
 <template>
-    <div class="notes mx-5 mt-5 mb-5">
-  
-      <div class="card has-background-link-dark p-4 mb-5">
-        <div class="field">
-          <div class="control">
-            <textarea
-              class="textarea"
-              placeholder="Add a new note"
-            />
-          </div>
-        </div>
-  
-        <div class="field is-grouped is-grouped-right">
-          <div class="control">
-            <button class="button is-link has-background-link">
-              Add New Note
-            </button>
-          </div>
+  <div class="notes mx-5 mt-5 mb-5">
+    <div class="card has-background-link-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea
+            class="textarea"
+            placeholder="Add a new note"
+            v-model="newNoteContent"
+          ></textarea>
         </div>
       </div>
-  
-      <div
-        v-for="i in 3"
-        class="card mb-4"
-      >
-        <div class="card-content">
-          <div class="content">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.
-          </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-link" @click="addNote">
+            Add New Note
+          </button>
         </div>
-        <footer class="card-footer">
-          <a href="#" class="card-footer-item">Edit</a>
-          <a href="#" class="card-footer-item">Delete</a>
-        </footer>
       </div>
     </div>
-  </template>
+
+    <div
+      v-for="note in notes"
+      :key="note.id"
+      class="card mb-4"
+    >
+      <div class="card-content">
+        <div class="content">
+          {{ note.content }}
+        </div>
+      </div>
+
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item" @click="editNote(note)">Edit</a>
+        <a href="#" class="card-footer-item" @click="deleteNote(note.id)">Delete</a>
+      </footer>
+    </div>
+  </div>
+</template>
+
+    <script>
+    import { ref } from 'vue'
+    const notes = ref([
+    {
+        id:'id1',
+        content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.'
+    },
+    {
+        id:'id2',
+        content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.'
+    }]
+)
+
+
+    </script>
