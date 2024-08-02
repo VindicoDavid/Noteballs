760647e8e69ea95022f512cde66bea32031686aa
David Diaz Clifton <daviddiaz-clifton@vindico.net>
2024-08-02 10:48:37 +0200

    Setup Pinia 92 and 93

--- Noteballs/src/main.js
+++ Noteballs/src/main.js
@@ -1,8 +1,10 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from '@/router';
 
-import { createApp } from 'vue'
+const app = createApp(App);
 
-import App from './App.vue'
-import router from '@/router/index.js'
-
-
-createApp(App).use(router).mount('#app')
+app.use(createPinia());
+app.use(router);
+app.mount('#app');
--- Noteballs/src/views/ViewNotes.vue
+++ Noteballs/src/views/ViewNotes.vue
@@ -20,7 +20,7 @@
       </div>
     </div>
     <Note
-    v-for="note in notes"
+    v-for="note in storeNotes.notes"
     :key="note.id"
     :note="note"
     class="card mb-4"
@@ -34,20 +34,14 @@
 <script setup>
 import { ref } from 'vue'
 import Note from '@/components/Notes/Note.vue'
+import {useStoreNotes} from '@/stores/storeNotes.js'
+
+const storeNotes = useStoreNotes()
 
 const newNote = ref('')
 const newNoteRef = ref(null)
 
-const notes = ref([
-  {
-    id: 'id1',
-    content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.'
-  },
-  {
-    id: 'id2',
-    content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.'
-  }]
-)
+
 const addNote = () => {
   let currentDate = new Date().getTime()
   let id = currentDate.toString()
