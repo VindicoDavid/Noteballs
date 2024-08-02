c00bf9f52df4b19f7f8ba7477002c42a309ae2d6
David Diaz Clifton <daviddiaz-clifton@vindico.net>
2024-08-02 17:16:50 +0200

    even target value needs to change not exisitng

--- Noteballs/src/components/Notes/AddEditNote.vue
+++ Noteballs/src/components/Notes/AddEditNote.vue
@@ -1,33 +1,32 @@
-<!-- edit form copied from add note -->
 <template>
-    <div class="card has-background-link-dark p-4 mb-5">
-      <div class="field">
-        <div class="control">
-          <textarea 
-            :value="modelValue" 
-            @input="$emit('update:modelValue', modelValue)" 
-            class="textarea" 
-            placeholder="Add a new note" 
-            ref="newNoteRef">
-          </textarea>
-        </div>
-      </div>
-  
-      <div class="field is-grouped is-grouped-right">
-        <div class="control">
-          <slot name="buttons"></slot>
-        </div>
+  <div class="card has-background-link-dark p-4 mb-5">
+    <div class="field">
+      <div class="control">
+        <textarea 
+          :value="modelValue" 
+          @input="$emit('update:modelValue', $event.target.value)" 
+          class="textarea" 
+          placeholder="Add a new note" 
+          ref="newNoteRef">
+        </textarea>
       </div>
     </div>
-  </template>
-  
+
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <slot name="buttons"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup>
 const emits = defineEmits(['update:modelValue'])
 
 const props = defineProps({
- modelValue:{
+  modelValue:{
     type: String,
     required: true
- }
+  }
 })
 </script>
