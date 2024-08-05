a676219f2620d4deefefd1e9791663e8ae879333
David Diaz Clifton <daviddiaz-clifton@vindico.net>
2024-08-05 19:31:43 +0200

    Add Delete Modal

--- Noteballs/src/components/Notes/Note.vue
+++ Noteballs/src/components/Notes/Note.vue
@@ -1,56 +1,81 @@
-
 <template>
-<div class="card mb-4">
+  <div
+    class="card mb-4"
+  >
     <div class="card-content">
       <div class="content">
         {{ note.content }}
-      </div>
-      <div class="has-text-right hast-text-grey light mt-2">
-        <small>{{ characterLength }}</small>
+        <div class="has-text-right has-text-grey-light mt-2">
+          <small>{{ characterLength }}</small>
         </div>
+      </div>
     </div>
-
     <footer class="card-footer">
-      <RouterLink 
-      :to="`/editNote/${props.note.id}`"
-      class="card-footer-item" 
-      @click.prevent="editNote">
-      Edit
-    </RouterLink>
-      <a href="#" class="card-footer-item" @click.prevent="storeNotes.deleteNote(props.note.id)">Delete</a>
+      <RouterLink
+        :to="`/editNote/${ note.id }`"
+        class="card-footer-item"
+        href="#"
+      >
+        Edit
+      </RouterLink>
+      <a
+        @click.prevent="modals.deleteNote = true"
+        class="card-footer-item"
+        href="#"
+      >
+        Delete
+      </a>
     </footer>
+    <ModalDeleteNote
+      v-if="modals.deleteNote"
+      v-model="modals.deleteNote"
+      :noteId="note.id"
+    />
   </div>
-  </template>
-  <script setup>
-    import { ref, computed } from 'vue'
-    import {useStoreNotes} from '@/stores/storeNotes.js'
+</template>
 
-    const storeNotes = useStoreNotes()
+<script setup>
+/*
+  imports
+*/
 
-    const characterLength = computed(() => {
-        let length = props.note.content.length
-        let description = length>1? 'characters': 'character'
-      return `${length} ${description}`
-    })
+  import { computed, reactive } from 'vue'
+  import ModalDeleteNote from '@/components/Notes/ModalDeleteNote.vue'
+  import { useStoreNotes } from '@/stores/storeNotes'
 
+/*
+  props
+*/
 
-    const props = defineProps({
-      note: Object,
+  const props = defineProps({
+    note: {
       type: Object,
       required: true
-    })
-    const emit = defineEmits([
-        'deleteClicked',
-        'editClicked'
-    ]
-       
-    )
-
-    const editNote = () => {
-       emit('editClicked', props.note.id)
     }
+  })
 
-    const handleDeleteClicked = () => {
-        emit('deleteClicked', props.note.id)
-    }
+/*
+  store
+*/
+
+  const storeNotes = useStoreNotes()
+
+/*
+  character length
+*/
+
+  const characterLength = computed(() => {
+    let length = props.note.content.length
+    let description = length > 1 ? 'characters' : 'character'
+    return `${ length } ${ description }`
+  })
+
+/*
+  modals
+*/
+
+  const modals = reactive({
+    deleteNote: false
+  })
+
 </script>
