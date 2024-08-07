0e4686de9c2e4d24b4bc457d99e18b707e557b25
David Diaz Clifton <daviddiaz-clifton@vindico.net>
2024-08-07 13:15:58 +0200

    create app and install firebase

---
+++ Noteballs/src/js/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAc6P1ct5nVif3Y7SafYsWQVyxllwFeSj4",
+  authDomain: "noteballs-a4e1b.firebaseapp.com",
+  projectId: "noteballs-a4e1b",
+  storageBucket: "noteballs-a4e1b.appspot.com",
+  messagingSenderId: "893327248924",
+  appId: "1:893327248924:web:f79edbc793f742b22519d9"
+};
+
+
+const app = initializeApp(firebaseConfig)
